dada447f0b62cab86ecc79ad0114f154d60fe48e
Ali aGHasizadeh <me@aligh.pro>
2012-12-07 12:35:41 +0330

    Revert to OLD times :D

--- index.php
+++ index.php
@@ -20,76 +20,55 @@
 		</header>
 		<section class="content_container">
 			<div class="content w24 mla mra cover">
-				<?php
-					error_reporting(E_ALL ^ E_NOTICE);
-					function get_browsers($browser) {
-						$dir = "browsers/$browser";
-						$allowed_type = array('exe','msi');
-
-						$d = dir($dir);
-						while( $f = $d->read() ){
-							$file_type = end(explode('.',$f));
-							if( !in_array(strtolower($file_type),$allowed_type) ) continue;
-
-							$file_name = substr($f,0,-strlen($file_type)-1);
-							echo "$dir/$file_name.$file_type";				
-						}
-					}
-				 ?>
+				
 				<div class="chrome left">
-					<div class="hover_mode">
+					<a href="#">
 						<div class="icon"></div>
 						<h2>گوگل کروم</h2>
 						<p class="info">سریع ترین مرورگر موجود، پر قدرت، امن و محیط کاربری ساده. همین حالا امتحانش کنید.</p>
 						<p class="version">آخرین ورژن : 23</p>
-						<a href="http://www.google.com/chrome" class="website">اطلاعات بیشتر</a>
-						<a href="<?php get_browsers('chrome') ?>" class="download">دانلود مرورگر</a>
-
-					</div>
+						<p class="website">اطلاعات بیشتر</p>
+					</a>
 				</div>
 
 				<div class="firefox left">
-					<div class="hover_mode">
+					<a href="#">
 						<div class="icon"></div>
 						<h2>موزیلا فایرفاکس</h2>
 						<p class="info">مرورگری سریع و پرقدرت. فایرفاکس بر اساس امنیت بیشتر ساخته شده، با این مرورگر امنیت رو تجربه کنید.</p>
 						<p class="version">آخرین ورژن : 17</p>
-						<a href="http://www.firefox.com/" class="website">اطلاعات بیشتر</a>
-						<a href="<?php get_browsers('firefox') ?>" class="download">دانلود مرورگر</a>
-					</div>
+						<p class="website">اطلاعات بیشتر</p>
+					</a>
 				</div>
 
 				<div class="safari left">
-					<div class="hover_mode">
+					<a href="#">
 						<div class="icon"></div>
 						<h2>سافاری</h2>
 						<p class="info">مرورگر سافاری محصول کمپانی اپل می باشد، که اکثر کاربرهای این مرورگر از سیستم عامل مک استفاده میکنند</p>
 						<p class="version">آخرین ورژن : 6</p>
-						<a href="http://www.apple.com/safari/" class="website">اطلاعات بیشتر</a>
-						<a href="<?php get_browsers('safari') ?>" class="download">دانلود مرورگر</a>
-					</div>
+						<p class="website">اطلاعات بیشتر</p>
+					</a>
 				</div>
 
 				<div class="opera left">
-					<div class="hover_mode">
+					<a href="#">
 						<div class="icon"></div>
 						<h2>اپرا</h2>
 						<p class="info">مرورگری پرسرعت، به راحتی قابل استفاده و دارای امنیت بالا</p>
 						<p class="version">آخرین ورژن : 12</p>
-						<a href="http://www.opera.com/" class="website">اطلاعات بیشتر</a>
-						<a href="<?php get_browsers('opera') ?>" class="download">دانلود مرورگر</a>
-					</div>
+						<p class="website">اطلاعات بیشتر</p>
+					</a>
 				</div>
 
 				<div class="iE last">
-					<div class="hover_mode">
+					<a href="#">
 						<div class="icon"></div>
 						<h2>اینترنت اکسپلورر</h2>
 						<p class="info">مرورگری از کمپانی مایکروسافت.</p>
 						<p class="version">آخرین ورژن : 10</p>
-						<a href="http://windows.microsoft.com/ie" class="website">اطلاعات بیشتر</a>
-						<a href="<?php get_browsers('ie') ?>" class="download">دانلود مرورگر</a>
-					</div>
+						<p class="website">اطلاعات بیشتر</p>
+					</a>
 				</div>
 
 			</div>
